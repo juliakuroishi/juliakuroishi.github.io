eab272372495ffab0fc91cfd7dd9dbfe0c379802
Julia Kuroishi <juliabkuroishi@gmail.com>
2023-05-15 15:55:18 -0400

    grandes alterações

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-alert("Porfólio ainda em construção :)");
+//alert("Porfólio ainda em construção :)");
+
+function functionQuemSouEu() {
+    var quemsou = document.getElementById("quemsoueu");
+    quemsou.hidden = false;
+}
+
+function functionHabilidades() {
+    var habilidades = document.getElementById("habilidades");
+    habilidades.hidden = false;
+}
+
+function functionMeuContato() {
+    var contato = document.getElementById("meucontato");
+    contato.hidden = false;
+}
