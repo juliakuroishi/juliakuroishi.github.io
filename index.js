701c9d86982c44df35cfbf4978a6fca4c4dc4c2a
Julia Kuroishi <juliabkuroishi@gmail.com>
2023-05-19 09:57:25 -0400

    correção do js

--- index.js
+++ index.js
@@ -2,15 +2,30 @@
 
 function functionQuemSouEu() {
     var quemsou = document.getElementById("quemsoueu");
-    quemsou.hidden = false;
+    var habilidades = document.getElementById("habilidades");
+    var contato = document.getElementById("meucontato");
+
+    contato.hidden = true;
+    quemsou.hidden = false; //o que vai aparecer somente
+    habilidades.hidden = true;
 }
 
 function functionHabilidades() {
+    var quemsou = document.getElementById("quemsoueu");
     var habilidades = document.getElementById("habilidades");
-    habilidades.hidden = false;
+    var contato = document.getElementById("meucontato");
+
+    contato.hidden = true;
+    quemsou.hidden = true;
+    habilidades.hidden = false; //o que vai aparecer somente
 }
 
 function functionMeuContato() {
+    var quemsou = document.getElementById("quemsoueu");
+    var habilidades = document.getElementById("habilidades");
     var contato = document.getElementById("meucontato");
-    contato.hidden = false;
+
+    contato.hidden = false; //o que vai aparecer somente
+    quemsou.hidden = true;
+    habilidades.hidden = true;
 }
